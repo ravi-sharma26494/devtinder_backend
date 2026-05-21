76a4959be33a18d93a0843d3c43b995412774314
Ravi Sharma <ravi.sharma26494@gmail.com>
2026-05-21 23:56:21 +0530

    Refactor package.json scripts and format app.js for consistency

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,28 +7,28 @@ const app = express();
 app.use(express.json());
 
 app.use(
-    "/user",
-    (req, res, next)=>{
-        console.log("Handling route 1 !!");
-        next();
-        // res.send("Hello from route 1 !!");
-    },,
-    (req, res, next)=>{
-        console.log("Handling route 2 !!");
-        next();
-        // res.send("Hello from route 2 !!");
-    },
-    (req, res, next)=>{
-        console.log("Handling route 3 !!");
-        next();
-        // res.send("Hello from route 3 !!");
-    },
-    (req, res, next)=>{
-        console.log("Handling route 4 !!");
-        next();
-        // res.send("Hello from route 4!!");
-    }
-)
+  "/user",
+  (req, res, next) => {
+    console.log("Handling route 1 !!");
+    next();
+    // res.send("Hello from route 1 !!");
+  },
+  (req, res, next) => {
+    console.log("Handling route 2 !!");
+    next();
+    // res.send("Hello from route 2 !!");
+  },
+  (req, res, next) => {
+    console.log("Handling route 3 !!");
+    next();
+    // res.send("Hello from route 3 !!");
+  },
+  (req, res, next) => {
+    console.log("Handling route 4 !!");
+    next();
+    // res.send("Hello from route 4!!");
+  },
+);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
