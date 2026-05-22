810437efeb2963267bfad09b78dbd980cfde639d
Ravi Sharma <ravi.sharma26494@gmail.com>
2026-05-23 00:00:23 +0530

    Add dotenv for environment variable management and implement user signup endpoint

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
 const express = require("express");
-
+require("dotenv").config();
+const connectDB = require("./config/database");
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const userData = req.body;
+  console.log(userData);
+  // creating a new instance of the User model with the provided user data
+  const user = new User(userData);
+  await user.save();
+
+  res
+    .status(200)
+    .json({ message: "User data received successfully", data: userData });
+});
+
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.use(express.json());
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling route 1 !!");
-    next();
-    // res.send("Hello from route 1 !!");
-  },
-  (req, res, next) => {
-    console.log("Handling route 2 !!");
-    next();
-    // res.send("Hello from route 2 !!");
-  },
-  (req, res, next) => {
-    console.log("Handling route 3 !!");
-    next();
-    // res.send("Hello from route 3 !!");
-  },
-  (req, res, next) => {
-    console.log("Handling route 4 !!");
-    next();
-    // res.send("Hello from route 4!!");
-  },
-);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING);
 };
-
-connectDB()
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
-// mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING);
+module.exports = connectDB;
+// connectDB()
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB:", err);
+//   });
